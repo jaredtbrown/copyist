c6112e57c6c9ad14bab06e51c864a033b9baa380
Jared Brown <brown.jared25@gmail.com>
2020-07-25 00:04:27 -0400

    Refactored app init

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,5 +1,9 @@
-export const APP_INIT = 'APP_INIT';
+// app init
+export const APP_INIT_REQUEST = 'APP_INIT_REQUEST';
+export const APP_INIT_SUCCESS = 'APP_INIT_SUCCESS';
+export const APP_INIT_FAILURE = 'APP_INIT_FAILURE';
 
+// current user
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 export const CLEAR_CURRENT_USER = 'CLEAR_CURRENT_USER';
 
--- src/actions/appInit.js
+++ src/actions/appInit.js
@@ -1,9 +1,23 @@
 import { setCurrentUser } from './currentUser';
 import { setTeam } from './team';
 import firebaseWrapper from '../helpers/firebaseWrapper';
+import { APP_INIT_REQUEST, APP_INIT_SUCCESS, APP_INIT_FAILURE } from './actionTypes';
+
+const appInitRequest = () => ({
+    type: APP_INIT_REQUEST,
+});
+
+const appInitSuccess = () => ({
+    type: APP_INIT_SUCCESS,
+});
+
+const appInitFailure = () => ({
+    type: APP_INIT_FAILURE,
+});
 
 export const appInitialize = (user) => {
     return async (dispatch) => {
+        dispatch(appInitRequest());
         const database = firebaseWrapper.firestore();
         try {
             const userDocument = await database.doc(`users/${user.uid}`).get();
@@ -17,8 +31,9 @@ export const appInitialize = (user) => {
             const teamDocument = await database.doc(`teams/${userData.teamId}`).get();
             const teamData = teamDocument.data();
             dispatch(setTeam(teamData));
+            dispatch(appInitSuccess());
         } catch (error) {
-            console.log('error');
+            dispatch(appInitFailure());
         }
     }
 }
